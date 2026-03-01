8800fdcf25117e27bae2d4838c80fe08059cb4ca
Ghent Starshadow <ghent@starshadow.com>
2026-03-01 11:13:28 -0500

    fix(toast): raise z-index to z-[100] to render above dialogs
    Toast popups were appearing blurred behind dialog/modal backdrops
    because both used z-50. Toasts now use z-[100] to always render
    on top of all layers.

--- frontend/app/components/ToastContainer.vue
+++ frontend/app/components/ToastContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed bottom-4 right-4 z-50 flex flex-col space-y-2 max-w-sm">
+  <div class="fixed bottom-4 right-4 z-[100] flex flex-col space-y-2 max-w-sm">
     <TransitionGroup
       enter-active-class="transition-all duration-300 ease-out"
       enter-from-class="translate-x-full opacity-0"
